901feba819913f7b32a2759e579f7a45204dd13e
Ben Nelson <nayongbin8@gmail.com>
2020-11-12 15:36:07 -0500

    Revert "Revert "OpenGL Vaos, Vbos, and Shaders""
    This reverts commit 2ab9b8bf5862ffec0692745f800f82e37363733d.

---
+++ src/com/darkerminecraft/shaders/entities/EntityShader.java
@@ -0,0 +1,11 @@
+package com.darkerminecraft.shaders.entities;
+
+import com.darkerminecraft.shaders.ShaderProgram;
+
+public class EntityShader extends ShaderProgram {
+
+	public EntityShader() {
+		super("entities", false, "position");
+	}
+
+}
